24c06e063c21c4dabc7736de8e23323005982d7b
ChaiyoKung <kananek45@gmail.com>
2025-06-14 23:45:45 +0700

    feat: configure JWT expiration time using environment variable

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,22 @@ import { LocalStrategy } from "./local.strategy";
 import { JwtModule } from "@nestjs/jwt";
 import { jwtConstants } from "./constants";
 import { JwtStrategy } from "./jwt.strategy";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: "60s" },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: jwtConstants.secret,
+        signOptions: {
+          expiresIn: configService.get<string>("JWT_EXPIRES_IN") || "60s",
+        },
+      }),
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
