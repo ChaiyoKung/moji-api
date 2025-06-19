176dba97cdb78a1e4c3fb27827de482386430c0c
ChaiyoKung <kananek45@gmail.com>
2025-06-19 22:56:50 +0700

    feat: user registration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { JwtModule } from "@nestjs/jwt";
 import { jwtConstants } from "./constants";
 import { JwtStrategy } from "./jwt.strategy";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthController } from "./auth.controller";
 
 @Module({
   imports: [
@@ -26,5 +27,6 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
